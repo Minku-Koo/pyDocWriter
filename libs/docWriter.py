3bea39d685c2b0f99dbefd6401ecf3925ba63242
Minku Koo <koomk97@naver.com>
2022-12-06 23:09:24 +0900

    Update docWriter.py

--- libs/docWriter.py
+++ libs/docWriter.py
@@ -10,18 +10,45 @@ from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QGridLayout, QLabel, QLineEdit, QTextEdit
 import os
+import time
 # import shutil
 # from PyQt5.Qt import Qt
 import datetime 
+from PyQt5 import uic
 
-# addWidget (self, QWidget, row, column, rowSpan, columnSpan, Qt.Alignment alignment = 0)
+
+class Worker(QThread):
+    def __init__(self, parents):
+        super().__init__(parents)
+        self.parents = parents
+        self.running = True
+
+    def run(self):
+        while self.running:
+            time.sleep(0.05)
+            if self.parents.scroll_flag:
+                self.scroll()
+            else:
+                self.stop()
+
+    def scroll(self):
+        self.parents.group_scroll_area.verticalScrollBar().setValue(
+            self.parents.group_scroll_area.verticalScrollBar().maximum()
+        )
+        self.parents.scroll_flag = False
+        return 
+
+    def stop(self):
+        self.running = False
+        self.quit()
+        return 
 
 
 class DocWriter(QWidget):
     def __init__(self):
         super().__init__()
         
-        self.HEIGHT = 600
+        self.HEIGHT = 700
         self.WIDTH = 600 * 2
         self.mark_num = 0
         self.logo_img_size = 80
@@ -38,14 +65,18 @@ class DocWriter(QWidget):
         self.excel_import_name = 'Import'
         self.excel_export_name = 'Export'
         self.log_init_comment = """This is Log View"""
-        self.makr_name = 'mark'
+        # self.makr_name = 'mark' 
         self.run_text = 'RUN'
+
+        self.scroll_flag = True
 
         self.title_font = QFont()
         self.title_font.setPointSize(30)
 
         self.initGUI()
         return 
+
+    
 
     def initGUI(self): # main user interface 
         self.setWindowTitle(self.title) #GUI Title
@@ -89,35 +120,33 @@ class DocWriter(QWidget):
 
     def guiControlPannel(self):
         self.groupbox = QGroupBox(self)
-        # vbox = QVBoxLayout()
-
-        # mark_box.addWidget(mark_box)
-        # vbox.setLayout(mark_box)
+        self.group_scroll_area = QScrollArea(self)
+        self.group_scroll_area.setWidgetResizable(True)
         
-        self.grid.addWidget(self.groupbox, 3, 0, 6, 5)
+        self.grid.addWidget(self.group_scroll_area, 3, 0, 5, 5)
         self.mark_vbox = QVBoxLayout()
 
         import_btn = QPushButton(self)
         import_btn.setText(self.excel_import_name)
-        self.grid.addWidget(import_btn, 9, 0, 1, 1)
+        self.grid.addWidget(import_btn, 8, 0, 1, 1)
 
         export_btn = QPushButton(self)
         export_btn.setText(self.excel_export_name)
-        self.grid.addWidget(export_btn, 9, 1, 1, 1)
+        self.grid.addWidget(export_btn, 8, 1, 1, 1)
 
         mark_plus_btn = QPushButton(self)
         mark_plus_btn.setText('+')
         mark_plus_btn.clicked.connect(self.generate_mark)
-        self.grid.addWidget(mark_plus_btn, 9, 2, 1, 1)
+        self.grid.addWidget(mark_plus_btn, 8, 4, 1, 1)
 
         run_btn = QPushButton(self)
         run_btn.setText(self.run_text)
-        self.grid.addWidget(run_btn, 9, 4, 1, 1)
+        self.grid.addWidget(run_btn, 9, 1, 1, 3)
 
-        
-        self.generate_mark()
-        
         self.groupbox.setLayout(self.mark_vbox)
+        self.group_scroll_area.setWidget(self.groupbox)
+
+        self.generate_mark()
         return 
 
     def guiLogView(self):
@@ -129,7 +158,7 @@ class DocWriter(QWidget):
         log_view.append(self.log_init_comment)
         # self.tb.setAcceptRichText(True)
         # self.tb.setOpenExternalLinks(True)
-        self.grid.addWidget(log_view, 1, 6, 8, 4)
+        self.grid.addWidget(log_view, 1, 6, 7, 4)
 
         logo_img = QPixmap(self.logo_file).scaled(self.logo_img_size, self.logo_img_size)
         logo_img_box = QLabel()
@@ -138,28 +167,49 @@ class DocWriter(QWidget):
 
         return 
 
+    # + click event
     def generate_mark(self):
-        self.mark_num += 1
+        self.create_mark()
 
+        self.scroll_flag = True
+        self.worker = Worker(self)
+        self.worker.start()
+
+        # self.set_scroll_to_down()
+        
+        return 
+
+
+    def set_scroll_to_down(self):
+        self.group_scroll_area.verticalScrollBar().setValue(
+            self.group_scroll_area.verticalScrollBar().maximum()
+        )
+        return 
+
+    def create_mark(self):
+        self.mark_num += 1
         mark_box = QHBoxLayout()
+
         mark_line_num = QLineEdit(self)
-        mark_line_num.setText(f"{self.makr_name}{self.mark_num}")
-        # mark_line_num.setFixedWidth(80)
+        mark_line_num.setText(f"{self.mark_num}")
         mark_line_name = QLineEdit(self)
-        # mark_line_num.setFixedWidth(200)
-        mark_line_name.setAlignment(Qt.AlignTop)
-        # mark_line_value = QTextEdit(self)
-        mark_line_value = QLineEdit(self)
-        mark_line_value.setFixedWidth(400)
-        # mark_line_value.setFixedHeight(40)
+        mark_line_value = QTextEdit(self)
+        mark_line_value.setFixedWidth(380)
+        mark_line_value.setFixedHeight(40)
+        rem_btn = QPushButton(self)
+        rem_btn.setText('X')
+        rem_btn.clicked.connect(self.remove_mark)
+        rem_btn.setFixedWidth(30)
+        rem_btn.setFixedHeight(30)
 
         mark_box.addWidget(mark_line_num)
         mark_box.addWidget(mark_line_name)
         mark_box.addWidget(mark_line_value)
+        mark_box.addWidget(rem_btn)
 
-        mark_box.setAlignment(Qt.AlignTop)
-        self.mark_vbox.setAlignment(Qt.AlignTop)
         self.mark_vbox.addLayout(mark_box)
+        self.mark_vbox.setAlignment(Qt.AlignTop)
+        
         return 
 
     def remove_mark(self):
@@ -167,7 +217,5 @@ class DocWriter(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # screen = app.primaryScreen()
     ex = DocWriter()
     sys.exit(app.exec_())
-    pass
