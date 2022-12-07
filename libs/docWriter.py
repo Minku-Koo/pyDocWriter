688cf9d0af395ae164de96d8685e79b30f595a47
Minku Koo <koomk97@naver.com>
2022-12-08 03:31:34 +0900

    Update docWriter.py

--- libs/docWriter.py
+++ libs/docWriter.py
@@ -1,5 +1,4 @@
 # -*- conding: utf-8 -*-
-
 # pip install pyqt5
 
 import sys
@@ -11,8 +10,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QGridLayout, QLabel, QLineEdit, QTextEdit
 import os
 import time
-# import shutil
-# from PyQt5.Qt import Qt
+import webbrowser
 import datetime 
 from PyQt5 import uic
 
@@ -52,6 +50,7 @@ class DocWriter(QWidget):
         self.WIDTH = 600 * 2
         self.mark_num = 0
         self.logo_img_size = 80
+        self.mark_input_height = 40
         self.resize(self.WIDTH, self.HEIGHT)
 
         self.title = "Ms Office Merger"
@@ -61,13 +60,31 @@ class DocWriter(QWidget):
         self.reset_btn_name = 'RESET'
         self.help_btn_name = 'HELP'
         self.doc_load_name = "File Load"
-        self.select_options_name = ('option1', 'option2')
+        self.select_options_name = ('새 문서에 작성', '기존 문서에 작성')
         self.target_path_btn_name = 'Target'
         self.excel_import_name = 'Import'
         self.excel_export_name = 'Export'
-        self.log_init_comment = """This is Log View"""
-        # self.makr_name = 'mark' 
+
+        self.log_comment = """This is Log View"""
+        self.file_loaded_log = '''MS file uploaded!'''
+        self.run_done = '''Work Done!'''
         self.run_text = 'RUN'
+        
+        self.ms_loaded_file_list = []
+        self.ms_loaded_file_index = 0
+        self.ms_loaded_file_label = {}  # filename : (rem_btn_label, fileLabel)
+
+        self.mark_obj_dict = {}             # markindex : (mark_line_num, mark_line_name, mark_line_value, rem_btn)
+        # self.mark_value_dict = {}           # markindex : 
+
+        self.output_target_path = './'
+
+        self.help_link_url = "https://www.naver.com/"
+        self.our_logo_link_url = 'https://html-color-codes.info/Korean/'
+
+        self.target_active_style = 'border-style:solid;border-color:#000000;border-width:1px;'
+        self.target_unactive_style = 'border-style:solid;border-color:#c7c7c7;border-width:1px;'
+        self.no_border = 'border-width:0px;'
 
         self.scroll_flag = True
 
@@ -76,8 +93,6 @@ class DocWriter(QWidget):
 
         self.initGUI()
         return 
-
-    
 
     def initGUI(self): # main user interface 
         self.setWindowTitle(self.title) #GUI Title
@@ -101,29 +116,42 @@ class DocWriter(QWidget):
         title.setStyleSheet('border-style:solid;border-color:black;border-width:1px;')
         self.grid.addWidget(title, 0, 2, 1, 3)
         
-        doc_load_btn = QPushButton(self)
+        doc_load_btn = QPushButton(self.doc_load_name, self)
         doc_load_btn.setText(self.doc_load_name)
+        doc_load_btn.clicked.connect(self.load_file_list)
         self.grid.addWidget(doc_load_btn, 1, 0, 1, 1)
 
 
-        file_workspace = QLabel(self)
-        file_workspace.setStyleSheet('border-style:solid;border-color:black;border-width:1px;')
-        self.grid.addWidget(file_workspace, 2, 0, 2, 5)
+        self.file_workspace = QGroupBox(self)
+        self.file_workspace.setStyleSheet('border-style:solid;border-color:black;border-width:1px;')
+        
 
-        select_box = QComboBox(self)
-        select_box.addItem(self.select_options_name[0])
-        select_box.addItem(self.select_options_name[1])
-        self.grid.addWidget(select_box, 4, 0, 1, 1)
+        self.file_scroll_area = QScrollArea(self)
+        self.file_scroll_area.setWidgetResizable(True)
+        self.grid.addWidget(self.file_scroll_area, 2, 0, 2, 5)
 
-        target_btn = QPushButton(self)
-        target_btn.setEnabled(False)
-        target_btn.setText(self.target_path_btn_name)
-        self.grid.addWidget(target_btn, 4, 1, 1, 1)
+        self.file_vbox = QVBoxLayout()
 
-        target_path_bax = QLabel(self)
-        target_path_bax.setStyleSheet('border-style:solid;border-color:#c7c7c7;border-width:1px;')
-        target_path_bax.setText('')
-        self.grid.addWidget(target_path_bax, 4, 2, 1, 2)
+        self.file_workspace.setLayout(self.file_vbox)
+        self.file_scroll_area.setWidget(self.file_workspace)
+
+        self.select_box = QComboBox(self)
+        self.select_box.addItem(self.select_options_name[0])
+        self.select_box.addItem(self.select_options_name[1])
+        self.select_box.currentIndexChanged.connect(self.change_combo)
+        
+        self.grid.addWidget(self.select_box, 4, 0, 1, 1)
+
+        self.target_btn = QPushButton(self)
+        self.target_btn.setEnabled(True)
+        self.target_btn.setText(self.target_path_btn_name)
+        self.target_btn.clicked.connect(self.set_output_target_path)
+        self.grid.addWidget(self.target_btn, 4, 1, 1, 1)
+
+        self.target_path_box = QLabel(self)
+        self.target_path_box.setStyleSheet(self.target_active_style)
+        self.target_path_box.setText('')
+        self.grid.addWidget(self.target_path_box, 4, 2, 1, 2)
 
         return 
 
@@ -150,6 +178,7 @@ class DocWriter(QWidget):
 
         run_btn = QPushButton(self)
         run_btn.setText(self.run_text)
+        run_btn.clicked.connect(self.__run)
         self.grid.addWidget(run_btn, 10, 1, 1, 3)
 
         self.groupbox.setLayout(self.mark_vbox)
@@ -161,57 +190,54 @@ class DocWriter(QWidget):
     def guiLogView(self):
         help_btn = QPushButton(self)
         help_btn.setText(self.help_btn_name)
+        help_btn.clicked.connect(lambda: self.open_webbrowser(self.help_link_url))
         self.grid.addWidget(help_btn, 1, 8, 1, 1)
 
         init_btn = QPushButton(self)
         init_btn.setText(self.reset_btn_name)
         self.grid.addWidget(init_btn, 1, 9, 1, 1)
 
-        log_view = QTextBrowser()
-        log_view.append(self.log_init_comment)
+        self.log_view = QTextBrowser(self)
+        self.log_view.append(self.log_comment)
         # self.tb.setAcceptRichText(True)
         # self.tb.setOpenExternalLinks(True)
-        self.grid.addWidget(log_view, 2, 7, 7, 3)
+        self.grid.addWidget(self.log_view, 2, 7, 7, 3)
 
         logo_img = QPixmap(self.logo_file).scaled(self.logo_img_size, self.logo_img_size)
         logo_img_box = QLabel()
+        logo_img_box.resize(self.logo_img_size, self.logo_img_size)
         logo_img_box.setPixmap(logo_img)
+        # logo_img_box.setText(f'''<a href="{self.our_logo_link_url}"><img src="{self.logo_file}"
+        #                     width={self.logo_img_size} height={self.logo_img_size}></a>''')
         self.grid.addWidget(logo_img_box, 10, 9, 1, 1)
 
         return 
 
     # + click event
     def generate_mark(self):
-        self.create_mark()
+        self.mark_num += 1
+        self.create_mark(self.mark_num)
 
         self.scroll_flag = True
         self.worker = Worker(self)
         self.worker.start()
 
-        # self.set_scroll_to_down()
-        
         return 
 
-
-    def set_scroll_to_down(self):
-        self.group_scroll_area.verticalScrollBar().setValue(
-            self.group_scroll_area.verticalScrollBar().maximum()
-        )
-        return 
-
-    def create_mark(self):
-        self.mark_num += 1
+    def create_mark(self, mark_index):
         mark_box = QHBoxLayout()
 
         mark_line_num = QLineEdit(self)
-        mark_line_num.setText(f"mark{self.mark_num}")
+        mark_line_num.setText(f"mark{mark_index}")
+        mark_line_num.setFixedHeight(self.mark_input_height)
         mark_line_name = QLineEdit(self)
+        mark_line_name.setFixedHeight(self.mark_input_height)
         mark_line_value = QTextEdit(self)
         mark_line_value.setFixedWidth(380)
-        mark_line_value.setFixedHeight(40)
+        mark_line_value.setFixedHeight(self.mark_input_height)
         rem_btn = QPushButton(self)
         rem_btn.setText('X')
-        rem_btn.clicked.connect(self.remove_mark)
+        rem_btn.clicked.connect(lambda: self.remove_mark(mark_index))
         rem_btn.setFixedWidth(30)
         rem_btn.setFixedHeight(30)
 
@@ -222,10 +248,87 @@ class DocWriter(QWidget):
 
         self.mark_vbox.addLayout(mark_box)
         self.mark_vbox.setAlignment(Qt.AlignTop)
-        
+
+        self.mark_obj_dict[self.mark_num] = (
+                                            mark_line_num,
+                                            mark_line_name,
+                                            mark_line_value,
+                                            rem_btn)
         return 
 
-    def remove_mark(self):
+    def remove_mark(self, mark_index):
+        print(mark_index)
+        return 
+
+    def change_combo(self):
+        combo_idx = self.select_box.currentIndex()
+        if combo_idx == 1:
+            self.target_btn.setEnabled(False)
+            self.target_path_box.setStyleSheet(self.target_unactive_style)
+            self.target_path_box.setText('')
+        else:
+            self.target_btn.setEnabled(True)
+            self.target_path_box.setStyleSheet(self.target_active_style)
+            self.target_path_box.setText('')
+        return 
+
+    def load_file_list(self):
+        flist = QFileDialog.getOpenFileNames(self, 'Open file', './', 'ms file(*.xlsx *.xls *.docx)')
+        for filename in flist[0]:
+            self.create_file_list_label(filename)
+        self.add_log(self.file_loaded_log)
+        return 
+
+    def create_file_list_label(self, filename):
+        file_group_box = QHBoxLayout()
+
+        rem_btn = QPushButton(self)
+        rem_btn.setText('X')
+        rem_btn.clicked.connect(lambda: self.remove_file_list(filename))
+        rem_btn.setFixedWidth(20)
+        rem_btn.setFixedHeight(20)
+
+        file_label = QLabel(self)
+        file_label.setText(filename)
+        file_label.setStyleSheet(self.no_border)
+
+        file_group_box.addWidget(rem_btn)
+        file_group_box.addWidget(file_label)
+
+        self.file_vbox.addLayout(file_group_box)
+        self.file_vbox.setAlignment(Qt.AlignTop)
+
+        self.ms_loaded_file_list.append(filename)
+        self.ms_loaded_file_index += 1
+        self.ms_loaded_file_label[filename] = (rem_btn, file_label)
+        return 
+
+    def remove_file_list(self, filename):
+        self.file_vbox.removeWidget(self.ms_loaded_file_label[filename][0])
+        self.file_vbox.removeWidget(self.ms_loaded_file_label[filename][1])
+
+        del self.ms_loaded_file_label[filename]
+        self.ms_loaded_file_list.remove(filename)
+        return 
+
+    def set_output_target_path(self):
+        self.output_target_path = QFileDialog.getExistingDirectory(self, 'Select Directory', './')
+        self.target_path_box.setText(self.output_target_path)
+        return 
+
+    def open_webbrowser(self, url):
+        webbrowser.open(url)
+        return 
+
+    def add_log(self, text):
+        self.log_view.append(text)
+        return 
+
+    def __run(self):
+        for mark_number in range(1, self.mark_num + 1):
+            lb_name = self.mark_obj_dict[mark_number][1]
+            lb_value = self.mark_obj_dict[mark_number][2]
+        self.add_log(self.run_done)
         return 
 
 if __name__ == '__main__':
