55b40d0536882e8e78c744c953b2811f70c45506
Minku Koo <koomk97@naver.com>
2023-02-14 23:51:04 +0900

    Update xlDiffSetColor.py

--- xlDiffBgColor/xlDiffSetColor.py
+++ xlDiffBgColor/xlDiffSetColor.py
@@ -1,8 +1,10 @@
 import xlwings as xw
 import pandas as pd
 from pyxlsb import open_workbook as open_xlsb
+import openpyxl as xl
+from openpyxl.styles import PatternFill
 
-
+'''
 def _open_excel_wlwings(filepath, sheetname):
     # 엑셀 인스턴스 생성
     app = xw.App(visible=True)
@@ -52,7 +54,56 @@ if __name__ == "__main__":
 
     t_book, t_sheet = _open_excel_wlwings(t, sheet_name)
 
-    # s_sheet.range(5,5).color = color
-
     for y, x in diff_tuple:
         t_sheet.range(y, x).color = color
+'''
+
+def _open_excel_openpyxl(filepath, sheetname):
+    # Load workbook
+    book = xl.load_workbook(filepath)
+    # Access to a worksheet named 'no_header'
+    sheet = book[sheetname]
+    return book, sheet
+
+def get_df_pandas(filepath, sheetname):
+    # Load workbook
+    book = xl.load_workbook(filepath)
+    # Access to a worksheet named 'no_header'
+    sheet = book[sheetname]
+    # Convert to DataFrame
+    df = pd.DataFrame(sheet.values)
+    return df
+
+def _compare_df(df1, df2):
+    result = []
+    for x, _ in enumerate(df1.iterrows()):
+        for y, _ in enumerate(df1.iloc[x]):
+            if df1.iloc[x][y] == df2.iloc[x][y]:
+                continue
+            print(y, x)
+            result.append((y, x))
+    return result
+
+if __name__ == "__main__":
+    s = "./sample/1.xlsx"
+    t = "./sample/3.xlsx"
+
+    sheet_name = 'sh2'
+
+    sb, ss = _open_excel_openpyxl(s, sheet_name)
+    tb, ts = _open_excel_openpyxl(t, sheet_name)
+
+    
+
+    diff_tuple = _compare_df(pd.DataFrame(ss.values), pd.DataFrame(ts.values))
+
+    print(pd.DataFrame(ts.values))
+
+    color = 'FAAC58'
+    bfill = PatternFill(start_color=color, end_color=color, fill_type='solid')
+
+    for y, x in diff_tuple:
+        ts.cell(row=y, column=x).fill = bfill
+
+    # tb.template = False
+    tb.save(t)
